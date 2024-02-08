833307a8f90851c6c63458bfec1ab156e47638dc
James Gallagher <jamesg@jamesg.blog>
2024-02-08 09:09:36 +0000

    remove unused files, run black

--- autodistill_bioclip/bioclip_model.py
+++ autodistill_bioclip/bioclip_model.py
@@ -1,14 +1,13 @@
 import os
 from dataclasses import dataclass
-import open_clip
-
-import torch
-
-from autodistill.helpers import load_image
-import supervision as sv
-import numpy as np
-from autodistill.detection import CaptionOntology, DetectionBaseModel
 from typing import Any
+
+import numpy as np
+import open_clip
+import supervision as sv
+import torch
+from autodistill.detection import CaptionOntology, DetectionBaseModel
+from autodistill.helpers import load_image
 
 HOME = os.path.expanduser("~")
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -17,11 +16,13 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 @dataclass
 class BioCLIP(DetectionBaseModel):
     ontology: CaptionOntology
-    
+
     def __init__(self, ontology: CaptionOntology):
-        model, _, preprocess = open_clip.create_model_and_transforms('hf-hub:imageomics/bioclip')
-        tokenizer = open_clip.get_tokenizer('hf-hub:imageomics/bioclip')
-        
+        model, _, preprocess = open_clip.create_model_and_transforms(
+            "hf-hub:imageomics/bioclip"
+        )
+        tokenizer = open_clip.get_tokenizer("hf-hub:imageomics/bioclip")
+
         self.model = model.to(DEVICE)
         self.preprocess = preprocess
         self.tokenizer = tokenizer
@@ -30,13 +31,15 @@ class BioCLIP(DetectionBaseModel):
 
     def predict(self, input: Any, confidence: int = 0.5) -> sv.Detections:
         classes = self.ontology.prompts()
-        
+
         image = load_image(input, return_format="PIL")
         image = self.preprocess(image).unsqueeze(0).to(DEVICE)
 
         with torch.no_grad():
             image_features = self.model.encode_image(image)
-            text_features = self.model.encode_text(self.tokenizer([f"This is a photo of a {class_}" for class_ in classes]))
+            text_features = self.model.encode_text(
+                self.tokenizer([f"This is a photo of a {class_}" for class_ in classes])
+            )
             image_features /= image_features.norm(dim=-1, keepdim=True)
             text_features /= text_features.norm(dim=-1, keepdim=True)
 
