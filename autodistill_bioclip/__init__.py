1ab327418f1dd880279388faab4ffcdac8608ebd
James Gallagher <jamesg@jamesg.blog>
2024-02-08 09:09:21 +0000

    add bioclip code

---
+++ autodistill_bioclip/__init__.py
@@ -0,0 +1,3 @@
+from autodistill_bioclip.bioclip_model import BioCLIP
+
+__version__ = "0.1.0"
---
+++ autodistill_bioclip/bioclip_model.py
@@ -0,0 +1,50 @@
+import os
+from dataclasses import dataclass
+import open_clip
+
+import torch
+
+from autodistill.helpers import load_image
+import supervision as sv
+import numpy as np
+from autodistill.detection import CaptionOntology, DetectionBaseModel
+from typing import Any
+
+HOME = os.path.expanduser("~")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+@dataclass
+class BioCLIP(DetectionBaseModel):
+    ontology: CaptionOntology
+    
+    def __init__(self, ontology: CaptionOntology):
+        model, _, preprocess = open_clip.create_model_and_transforms('hf-hub:imageomics/bioclip')
+        tokenizer = open_clip.get_tokenizer('hf-hub:imageomics/bioclip')
+        
+        self.model = model.to(DEVICE)
+        self.preprocess = preprocess
+        self.tokenizer = tokenizer
+
+        self.ontology = ontology
+
+    def predict(self, input: Any, confidence: int = 0.5) -> sv.Detections:
+        classes = self.ontology.prompts()
+        
+        image = load_image(input, return_format="PIL")
+        image = self.preprocess(image).unsqueeze(0).to(DEVICE)
+
+        with torch.no_grad():
+            image_features = self.model.encode_image(image)
+            text_features = self.model.encode_text(self.tokenizer([f"This is a photo of a {class_}" for class_ in classes]))
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+
+            text_probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+
+            print(np.array(text_probs.cpu().numpy()))
+
+            return sv.Classifications(
+                class_id=np.array([i for i in range(len(classes))]),
+                confidence=text_probs.cpu().numpy()[0],
+            )
